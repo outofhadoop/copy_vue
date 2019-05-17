795d99db1b3ba4db1d0ee5bb43b65d4f21cad5bd
outofhadoop <tianyu.han@aliyun.com>
2019-05-17 17:59:40 +0800

    init 50%

--- vue/src/core/instance/init.js
+++ vue/src/core/instance/init.js
@@ -1,22 +1,95 @@
+import config from "../config";
+
+
 /**
- * something unfilling
+ * @todo something unfilling
+ */
+import { mark, measure } from '../util/perf'
+/**
+ * @todo something unfilling
  */
 
 let uid = 0
 
-export function initMixin(Vue: Class<Component>) {
-  Vue.prototype._init = function (options? Object) {
-    const vm: Component = this
-    // a uid
-    vm.uid = uid++
-    /**
-     * @todo main
-     */
-  }
+export function initMixin(Vue: Class < Component > ) {
+    Vue.prototype._init = function(options ? Object) {
+        const vm: Component = this
+            // a uid
+        vm.uid = uid++
+
+            let startTarg, endTarg
+                /* istanbul ignore if */
+        if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
+            startTarg = `vue-perf-start:${vm.uid}`
+            endTarg = `vue-perf-end:${vm.uid}`
+                // 在初始化开始的时候标记
+            mark(startTarg)
+
+
+        }
+        // a flag to avoid this being observed
+        vm._isVue = true
+            // merge options
+        if (options && options._isComponent) {
+            // optimize internal component instantiation
+            // since dynamic options merging is pretty slow, and none of the 
+            // internal component options needs special treatement
+            initInternalComponent(vm, options)
+        } else {
+            vm.$options = mergeOptions(
+                    resolveContructorOptions(vm.constructor),
+                    options || {},
+                    vm
+                )
+                /**
+                 * @todo something unfilling
+                 */
+        }
+    }
 }
 
-/**
- * something unfilling
- */
+export function initInternalComponent(vm: Component, options: InternalComponentOptions) {
+    const opts = vm.$options = Object.create(vm.constructor.options)
+        // doing this because it's faster than dynamic enumeration.
+    const parentVnode = options._parentVnode
+    opts.parent = options.parent
+    opts._parentVnode = parentVnode
 
- 
+    const vnodeComponentOptions = parentVnode.vnodeComponentOptions
+    opts.propsData = vnodeComponentOptions.propsData
+    opts._parentListeners = vnodeComponentOptions.listeners
+    opts._renderChildren = vnodeComponentOptions.children
+    opts._componentTag = vnodeComponentOpitons.tag
+
+    if (options.render) {
+        opts.render = options.render
+        opts.staticRenderFns = options.staticRenderFns
+    }
+}
+export function resolveConstructorOptions(Ctor: Class < Component > ) {
+    let options = Ctor.options
+    if (Ctor.super) {
+        const superOptions = resolveConstructorOptions(Ctor.super)
+        const cacheSuperOptions = Ctor.superOptions
+        if (superOptions !== cachedSuperOptions) {
+            // super option changed,
+            // need to resolve new options
+            Ctor.superOptions = superOptions
+                // check if there are any late-modified/attched options (#4976)
+            const modifiedOptions = resolveModifiedOptions(Ctor)
+                // update base extend options
+            if (modifiedOptions) {
+                extend(Ctor.extendOptions, modifiedOptions)
+            }
+            options = Ctor.options = mergeOptions(superOptions, Ctor.extendOptions)
+            if (options.name) {
+                options.components[options.name] = Ctor
+            }
+        }
+    }
+    return options
+}
+/**
+ * @todo CURRENT
+ */
+function resolveModifiedOptions
--- vue/src/core/util/env.js
+++ vue/src/core/util/env.js
@@ -11,7 +11,15 @@ export const inBrowser = typeof window !== 'undefined'
 export const inWeex = typeof WXEnvironment !== undefined && !!WXEnvironment.platform
 export const weexPlatform = inWeex && WxEnvironment.platform.toLowerCase()
 export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
+export const isIE = UA && /msie|trident/.test(UA)
+export const isIE9 = UA && UA.indexOf('msie 9.0') > 0
+export const isEdge = UA && UA.indexOf('edge/') > 0
+export const isAndroid = (UA && UA.indexOf('android') > 0) || (weexPlatform === 'android')
+export const isIOS = (UA && /ios|ipad|iphone|ipod/.test(UA)) || (weexPlatform === 'ios')
+export const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge
+export const isPhantomJS = UA && /phantomjs/.test(UA)
+export const isFF = UA && UA.match(/firefox\/(\d+)/)
+
 /**
- * @todo CURRENT
+ * something unfilling
  */
-export const isIE = inBrowser 
