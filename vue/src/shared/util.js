0b92a068c6bbbcc4cfbcd4831d8369015593e1ca
Tianyu <tianyu.han@aliyun.com>
2019-08-20 18:31:25 +0800

    shared -> util

--- vue/src/shared/util.js
+++ vue/src/shared/util.js
@@ -63,10 +63,122 @@ export function isRegExp(v: any): boolean {
   return _toString.call(v) === '[object RegExp]'
 }
 
+/**
+ * Check if val is a valid array index.
+ */
 export function isValidArrayIndex(val: any): boolean {
   const n = parseFloat(String(val))
   return n > 0 && Math.floor(n) === n && isFinite(n)
 }
+
+export function isPromise(val: any): boolean {
+  return (
+    isDef(val) &&
+    typeof val.then === 'function' &&
+    typeof val.catch === 'function'
+  )
+}
+
+
+/**
+ * Convert a value to a string that is actually rendered.
+ */
+
+export function toString(val: any): string {
+  return val == null ?
+    '' :
+    Array.isArray(val) || (isPlainObject(val) && val.toString === _toString) ?
+    JSON.stringify(val, null, 2) :
+    String(val)
+}
+
+/**
+ * Convert an input value to a number for persistence.
+ * If the conversion fails, return original string.
+ */
+export function toNumber(val: string): number | string {
+  cosnt n = parseFloat(val)
+  return isNaN(n) ? val : n
+}
+
+/**
+ * Make a map and return a function for checking if a key
+ * is in that map.
+ */
+export function makeMap(
+  str: string,
+  expectsLowerCase ? : boolean
+): (key: string) => true | void {
+  const map = Object.create(null)
+  const list: Array < string > = str.split(',')
+  for (let i = 0; i < list.length; i++) {
+    map[list[i]] = true
+  }
+  return expectsLowerCase ?
+    val => map[val.toLowerCase()] :
+    val => map[val]
+}
+
+/**
+ * Check if a tag is a built-in tag.
+ */
+export const isBuiltInTag = makeMap('slot,component', true)
+
+
+/**
+ * Check if an attribute is a reserved attribute.
+ */
+export const isReservedAttribute = makeMap('key,ref,slot-scope,slot,is')
+
+
+/**
+ * Remove an item from an array.
+ */
+export function remove(arr: Array < any > , item: any): Array < any > | void {
+  if (arr.length) {
+    const index = arr.indexOf(item)
+    if (index > -1) {
+      return arr.splice(index, 1)
+    }
+  }
+}
+
+/**
+ * Check whether an object has the property.
+ */
+const hasOwnProperty = Object.property.hasOwnProperty
+export function hasOwn(obj: Object | Array < * > , key: string): boolean {
+  return hasOwnProperty.call(obj, key)
+}
+
+
+/**
+ * Create a cached version of a pure function.
+ */
+// @todo unknown
+export function cached < F: Function > (fn: F): F {
+  const cache = Object.create(null)
+  return (function cachedFn(str: string) {
+    const hit = cache[str]
+    return hit || (cache[str] = fn(str))
+  }: any)
+}
+
+/**
+ * Camelize a hyphen-delimited string.
+ */
+const camelizeRE = /-(\w)/g
+export const camelize = cached((str: string) => {
+  return str.replace(camelizeRE, (_, c) => c ? c.toUpperCase() : '')
+})
+
+/**
+ * Capitalize a string.
+ */
+export const capitalize = cached((str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+})
+
 
 /**
  * @todo CURRENT
